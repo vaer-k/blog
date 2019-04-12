d04d18391e43e45abec37d4dc109b0f19c83b079
Vincent Raerek <vincent@monadnomad.net>
2019-04-11 18:45:56 -0700

    Set up default route to 404

--- src/clj/blog/routes/home.clj
+++ src/clj/blog/routes/home.clj
@@ -2,13 +2,14 @@
   (:require [blog.layout :as layout]
             [blog.db.core :as db]
             [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]
             [ring.util.http-response :as response]
             [clojure.java.io :as io]))
 
 (defn home-page [request]
   (layout/render request "home.html"))
 
-(defn construction [request]
+(defn construction [& request]
   (layout/render request "construction.html"))
 
 (defroutes home-routes
@@ -16,5 +17,6 @@
   (GET "/posts" request (construction request))
   (GET "/docs" []
        (-> (response/ok (-> "docs/docs.md" io/resource slurp))
-           (response/header "Content-Type" "text/plain; charset=utf-8"))))
+           (response/header "Content-Type" "text/plain; charset=utf-8")))
+  (route/not-found (construction)))
 
