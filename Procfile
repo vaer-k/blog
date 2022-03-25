cfc35fa5fc41c81abb915fe3bef5a50b7fbcf304
Vincent Raerek <vincent@monadnomad.dev>
2022-03-25 11:34:11 -0700

    Init phx app

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dclojure.main.report=stderr -cp target/uberjar/blog.jar clojure.main -m blog.core
--- env/prod/cljs/blog/app.cljs
+++
@@ -1,7 +0,0 @@
-(ns blog.app
-  (:require [blog.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
