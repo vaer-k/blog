8eacabadf64a2d4bafd8b9d86ced6459287d6707
Vincent Raerek <vincent@monadnomad.dev>
2022-04-04 09:35:16 -0700

    Change page title

--- lib/blog_web/controllers/page_controller.ex
+++ lib/blog_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule BlogWeb.PageController do
   use BlogWeb, :controller
 
   def index(conn, _params) do
+    conn =
+      conn
+      |> assign(:page_title, "Vincent Raerek on the Internet")
+      |> assign(:suffix, "")
+
     render(conn, "index.html")
   end
 end
